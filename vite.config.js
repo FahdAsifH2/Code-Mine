c0f312cc9d07e947742efc627c4f2cfe3d895c8c
fahd <fahdasif51@gmail.com>
2025-07-06 04:10:58 +0500

    ios css

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: './', // ✅ This fixes asset path issues on iPhone
+  plugins: [react(), tailwindcss()],
 })
